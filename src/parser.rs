b40556bcb2cf5e23a7561c8379b55433dbcff8b5
Jorge <jorgemg0327@gmail.com>
2020-11-01 15:02:43 -0500

    Work on parser and iterator

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+
 use std::fs::File;
 use std::io::BufReader;
 use std::str::FromStr;
@@ -8,27 +9,24 @@ use xml::reader::{EventReader, XmlEvent};
 
 #[derive(Debug)]
 enum Element {
+    Initialized,
     Node(Node),
     EndOfDocument,
     Ignored,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Node {
-    id: i32,
+    id: i64,
     lat: f64,
     lon: f64,
     tags: Vec<Tag>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Tag {
     key: String,
-    val: String,
-}
-
-struct Xml {
-    nodes: HashMap<i32, Node>,
+    value: String,
 }
 
 fn find_attribute<T>(name: &str, attributes: &Vec<OwnedAttribute>) -> Result<T, T::Err>
@@ -60,13 +58,6 @@ fn create_element(name: &str, attributes: Vec<OwnedAttribute>) {
 }
 
 fn match_event(event: XmlEvent) {
-    // let evt = match event {
-    //     XmlEvent::EndDocument => Element::EndOfDocument,
-    //     XmlEvent::StartElement {
-    //         name, attributes, ..
-    //     } => Element::Node(Node{}),
-    //     _ => Element::Ignored,
-    // };
     match event {
         XmlEvent::StartElement {
             name, attributes, ..
@@ -79,12 +70,79 @@ pub fn parse(path: &str) {
     let file = File::open(path).expect("Could not open xml file");
     let file = BufReader::new(file);
 
-    let parser = EventReader::new(file);
+    let mut parser = EventReader::new(file);
+    let mut ref_nodes: HashMap<i64, Node> = HashMap::new();
+    let mut nodes: HashMap<i64, Node> = HashMap::new();
 
-    for e in parser {
-        let event = e.unwrap();
-        match_event(event)
+    let mut current_element = Element::Initialized;
+    loop {
+        let evt = parser.next().expect("Parsing error!");
+        match evt {
+            XmlEvent::StartElement {
+                name, attributes, ..
+            } => match name.local_name.as_str() {
+                "node" => {
+                    let lat = find_attribute::<f64>("lat", &attributes).expect("Error parsing");
+                    let lon = find_attribute::<f64>("lon", &attributes).expect("Error parsing");
+                    let id = find_attribute::<i64>("id", &attributes).expect("Error parsing");
+
+                    let node = Node {
+                        id: id,
+                        lat: lat,
+                        lon: lon,
+                        tags: Vec::new(),
+                    };
+                    current_element = Element::Node(node);
+                }
+                // If there are tags...include them in the current element.
+                "tag" => {
+                    let key = attributes
+                        .iter()
+                        .find(|i| i.name.local_name == "k")
+                        .unwrap()
+                        .value
+                        .clone();
+
+                    let value = attributes
+                        .iter()
+                        .find(|i| i.name.local_name == "v")
+                        .unwrap()
+                        .value
+                        .clone();
+
+                    let tag = Tag {
+                        key: key,
+                        value: value,
+                    };
+
+                    match current_element {
+                        Element::Node(ref mut n) => n.tags.push(tag),
+                        _ => continue,
+                    }
+                }
+                _ => println!("{:?}", name),
+            },
+            XmlEvent::EndElement { name } => {
+                match name.local_name.as_str() {
+                    "node" => match current_element {
+                        Element::Node(ref n) => {
+                            if n.tags.len() == 0 {
+                                ref_nodes.insert(n.id, n.clone());
+                            } else {
+                                nodes.insert(n.id, n.clone());
+                            }
+                        }
+                        _ => continue,
+                    },
+                    _ => continue,
+                }
+                current_element = Element::Initialized;
+            }
+            XmlEvent::EndDocument => break,
+            _ => continue,
+        }
     }
+    println!("{:?}", nodes);
 }
 
 #[cfg(test)]
