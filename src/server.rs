9cf96704a311e98dae7107fbf8cd220025882bbd
Jorge Martinez <jorgemg0327@gmail.com>
2020-11-25 10:38:22 -0500

    Encode and decode signed hash

--- src/server.rs
+++ src/server.rs
@@ -9,6 +9,11 @@ use actix_web::{
 };
 use geojson;
 
+use base64::{decode, encode};
+use itsdangerous::{default_builder, Signer};
+
+const SECRET_KEY: &str = "pleasechangeme1234";
+
 #[derive(Clone)]
 struct AppState {
     storage: LocalStorage,
@@ -23,17 +28,46 @@ impl FromRequest for User {
     type Error = Error;
     type Future = std::pin::Pin<Box<dyn std::future::Future<Output = Result<User, Error>>>>;
 
-    fn from_request(req: &HttpRequest, pl: &mut Payload) -> Self::Future {
-        let value = "AAAA".to_string();
+    fn from_request(req: &HttpRequest, _pl: &mut Payload) -> Self::Future {
+        let auth = req
+            .headers()
+            .get("AUTHORIZATION")
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .split(" ")
+            .collect::<Vec<&str>>()[1]
+            .clone();
+
+        let auth = decode(auth).unwrap();
+        let auth = std::str::from_utf8(&auth).unwrap();
+
+        let signer = default_builder(SECRET_KEY).build();
+        let token = signer.unsign(auth).expect("errror").to_string();
+
         Box::pin(async move {
-            let user = User { token: value };
-            Err(ErrorUnauthorized("unauthorized"))
+            let user = User { token: token };
+            Ok(user)
+
+            //Err(ErrorUnauthorized("unauthorized"))
         })
     }
 }
 
+#[get("/token")]
+async fn get_token() -> String {
+    let signer = default_builder(SECRET_KEY).build();
+    let token = encode(signer.sign("12345"));
+
+    println!("{:?}", &token);
+
+    token
+}
+
 #[get("/campaigns")]
 async fn list_campaigns(user: User, data: web::Data<AppState>) -> Result<String, Notifications> {
+    println!("{}", user.token);
+
     let storage = &data.storage;
 
     let campaigns = std::fs::read_dir(&storage.path)?
@@ -61,6 +95,7 @@ pub async fn serve(storage: LocalStorage) -> Result<CommandResult, Notifications
             .wrap(Logger::default())
             .wrap(Logger::new("%a %{User-Agent}i"))
             .service(list_campaigns)
+            .service(get_token)
     })
     .bind("127.0.0.1:8080");
 
